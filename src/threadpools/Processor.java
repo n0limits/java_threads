25f835873aa6e7d8780e5e6d457ccc272d79fb4a
Viktor Grozev <viktor.grozev@sap.com>
2018-03-27 20:16:25 +0300

    adding java threads

---
+++ src/threadpools/Processor.java
@@ -0,0 +1,18 @@
+class Processor implements Runnable {
+
+    private int id;
+
+    public Processor(int id) {
+        this.id = id;
+    }
+
+    public void run() {
+        System.out.println("Starting: " + id);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Completed: " + id);
+    }
+}
